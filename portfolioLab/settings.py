67cb3f42b3d05a04d81c5487d4887b12fa5c26c2
wayzor9 <wayzor9@gmail.com>
2020-07-14 21:20:41 +0200

    01-fixing

--- portfolioLab/settings.py
+++ portfolioLab/settings.py
@@ -125,8 +125,8 @@ LOGOUT_REDIRECT_URL = 'login'
 AUTH_USER_MODEL = 'core.CustomUser'
 
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'wayzor9@gmail.com'
-EMAIL_HOST_PASSWORD = 'Homaribakardi7981'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
